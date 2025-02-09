dd48c7d4a7e4574c5ed7c366795468fb8e9c160e
lookerlukal <lookerlukal@gmail.com>
2025-02-10 02:47:05 +0800

    eee

--- file-viewer.php
+++ file-viewer.php
@@ -1,7 +1,8 @@
 <?php
 $file = $_GET['file'];
-if(preg_match('/\.INR$/', $file)) {
-    header('Content-Type: text/plain');
-    readfile($file);
+if(preg_match('/\.inr$/i', $file)) {
+    header('Content-Type: text/plain; charset=utf-8');
+    readfile(urldecode($file));
+    exit;
 }
 ?> 
